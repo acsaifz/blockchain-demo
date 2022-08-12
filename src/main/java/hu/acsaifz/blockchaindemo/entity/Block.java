3d6f6123cfbefdc484cc1ae16c21532d5e745265
acsaifz <angeluszmail@gmail.com>
2022-08-12 16:49:01 +0200

    update entity

--- src/main/java/hu/acsaifz/blockchaindemo/entity/Block.java
+++ src/main/java/hu/acsaifz/blockchaindemo/entity/Block.java
@@ -10,11 +10,11 @@ public class Block {
     private final LocalDateTime time;
     private final int proof;
 
-    public Block(long id, String previousHash, List<Transaction> transactionList, LocalDateTime time, int proof) {
+    public Block(long id, String previousHash, List<Transaction> transactionList, int proof) {
         this.id = id;
         this.previousHash = previousHash;
         this.transactionList = transactionList;
-        this.time = time;
+        this.time = LocalDateTime.now();
         this.proof = proof;
     }
 
@@ -36,5 +36,16 @@ public class Block {
 
     public int getProof() {
         return proof;
+    }
+
+    @Override
+    public String toString() {
+        return "Block{" +
+                "id=" + id +
+                ", previousHash='" + previousHash + '\'' +
+                ", transactionList=" + transactionList +
+                ", time=" + time +
+                ", proof=" + proof +
+                '}';
     }
 }
