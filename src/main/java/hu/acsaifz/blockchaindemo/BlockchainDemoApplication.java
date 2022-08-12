323addf0b5f8ca5b69a56ea4e95a82bb63fe9a1e
acsaifz <angeluszmail@gmail.com>
2022-08-12 11:35:50 +0200

    Initial commit

---
+++ src/main/java/hu/acsaifz/blockchaindemo/BlockchainDemoApplication.java
@@ -0,0 +1,13 @@
+package hu.acsaifz.blockchaindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainDemoApplication.class, args);
+    }
+
+}
